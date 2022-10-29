c410a812ee6c4ec41d0a79a96c7702e242972365
Alex Maystrenko <maystrenko_alex@mail.ru>
2022-10-29 23:02:36 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export const App = () => {
   const titleBtn_1 = 'inc';
   const titleBtn_2 = 'reset';
   const titleBtn_3 = 'set';
+  localStorage.setItem('isSettings', 'false')
 
   const [count, setCount] = useState<number>(0);
   const [start, setStart] = useState<number>(startvalue)
@@ -38,23 +39,23 @@ export const App = () => {
     
   }
  
-  const disabledStyleInc = (count === max) ? true : false;
-  const disabledStyleRes = (count === start && start !== max) ? true : false;
-  const disabledSettings = localStorage.getItem('isSettings') === 'true' ? true : false
+  // const disabledStyleInc = (count === max);
+  // const disabledStyleRes = (localStorage.getItem('isSettings') === 'false') || (count === start && start !== max) ? true : false;
+  // const disabledSettings = localStorage.getItem('isSettings') ? false : true
   
   return (
     <div className="App">
       <div className='counterWrapper'>
         <Display2x start={start} max={max} setStart={setStart} setMax={setMax} />
         <div className='buttonBlock'>
-          <Button name={titleBtn_3} disabled={disabledSettings} callback={setToLocalStorage} />
+          <Button name={titleBtn_3} disabled={false} callback={setToLocalStorage} />
         </div>
       </div>
       <div className='counterWrapper'>
         <Display count={count ? count : 'enter values and press key'} start={start} max={max}/>
         <div className='buttonBlock'>
-          <Button name={titleBtn_1} disabled={disabledStyleInc} callback={onClickUpCountHandler} />
-          <Button name={titleBtn_2} disabled={disabledStyleRes} callback={onClickDownCountHandler} />
+          <Button name={titleBtn_1} disabled={false} callback={onClickUpCountHandler} />
+          <Button name={titleBtn_2} disabled={false} callback={onClickDownCountHandler} />
         </div>
       </div>
     </div>
